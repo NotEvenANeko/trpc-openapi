f43600427f61bb9d53db00db4b03652930c39cfd
Neko <neko@qwq.icu>
2023-11-17 21:21:03 +0800

    chore: refactor tool chain

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,11 @@
-// @ts-check
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './test',
   snapshotFormat: {
     escapeString: true,
     printBasicPrototype: true,
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
 };
